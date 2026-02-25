16ffb257d35203bcc10f5cae52c7bcfdaeb96261
jonathan123-rgb <153829421+jonathan123-rgb@users.noreply.github.com>
2026-02-25 11:26:21 +0400

    Add superuser creation script to build process

--- demo/settings.py
+++ demo/settings.py
@@ -35,7 +35,7 @@ import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
